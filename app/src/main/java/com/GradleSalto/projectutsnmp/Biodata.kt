7b0047de7745de0566b06e466a0edd97fa8906b4
SmexyHarambe <s160423210@student.ubaya.ac.id>
2025-09-27 10:18:05 +0700

    Delete app directory

--- app/src/main/java/com/GradleSalto/projectutsnmp/Biodata.kt
+++
@@ -1,7 +0,0 @@
-package com.GradleSalto.projectutsnmp
-
-data class Biodata(
-    var nama: String,
-    var nrp: String,
-    var peminatan: String,
-    var imageID: Int)
--- app/src/main/java/com/GradleSalto/projectutsnmp/BiodataData.kt
+++
@@ -1,9 +0,0 @@
-package com.GradleSalto.projectutsnmp
-
-object BiodataData {
-    val biodata = arrayOf(
-        Biodata("Miranda Michelle Matulatan", "160423102", "NCS", R.drawable.foto_miranda_crop),
-        Biodata("Ferdinand Otniel Lundju", "160423214", "NCS", R.drawable.foto_ferdi_crop),
-        Biodata("Chrissatya Abel Cellonata", "160423210", "NCS", R.drawable.foto_abel_crop)
-    )
-}
